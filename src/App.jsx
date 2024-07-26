495d243d49d2e48219876e9433dbc3f935f6936b
Momina Babar <momina.babar@auxcube.com>
2024-07-26 13:36:38 +0500

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,110 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import Button from 'library/Button'
 
-const App = () => (
-  <Button
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+       <Button
     onClick={() => alert("Now everything's alright! If not, check reality settings.")}
   >
     Make everything alright
   </Button>
-)
+    </div>
+  );
+}
 
-ReactDOM.render(<App />, document.getElementById("app"))
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+        <Button
+    onClick={() => alert("Now everything's alright! If not, check reality settings.")}
+  >
+    Make everything alright
+  </Button>
+      </p>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("app")
+);
