7fa71d12f9379367f22e6c92a214adfb46524045
Momina Babar <momina.babar@auxcube.com>
2024-07-26 13:50:27 +0500

    change

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function Layout() {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/dashboard">Dashboardnnnnn</Link>
           </li>
           <li>
             <Link to="/nothing-here">Nothing Here</Link>
